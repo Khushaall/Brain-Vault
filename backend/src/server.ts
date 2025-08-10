4cd5fed34b0e325d7da628a23e747a61e65bde6c
Khushal Sharma <khushalsharma122@gmail.com>
2025-08-10 22:26:20 +0530

    Cors Change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://getbrainvault.netlify.app/ "
+    "https://getbrainvault.netlify.app"
 ];
 
 app.use(cors({
